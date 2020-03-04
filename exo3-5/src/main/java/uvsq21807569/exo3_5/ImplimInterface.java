51de949ed4ee5cc5016fca03f5f0e5273c2cf4fb
naitalamara <ouali.naitalamara@gmail.com>
2020-03-04 11:46:11 +0100

    ptt test

--- exo3-5/src/main/java/uvsq21807569/exo3_5/ImplimInterface.java
+++ exo3-5/src/main/java/uvsq21807569/exo3_5/ImplimInterface.java
@@ -1,5 +1,10 @@
 package uvsq21807569.exo3_5;
 
-public class ImplimInterface {
+public class ImplimInterface implements InterfaceAffich {
+
+	public void affichage(String s) {
+		System.out.println(s);
+		
+	}
 
 }
---
+++ exo3-5/src/test/java/uvsq21807569/exo3_5/Test.java
@@ -0,0 +1,17 @@
+package uvsq21807569.exo3_5;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		
+		ImplimInterface b = new ImplimInterface();
+		UneClasseMetier a = new UneClasseMetier();
+		
+		a.uneMethodeMetier(b);
+		
+	}
+
+}
